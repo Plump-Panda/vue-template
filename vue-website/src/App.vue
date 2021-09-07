bd1004041acd3bd7360625e834386054703ab1d8
Josh <js404839@dal.ca>
2021-09-07 12:15:21 -0700

    added background image

--- vue-website/src/App.vue
+++ vue-website/src/App.vue
@@ -15,11 +15,8 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('./assets/background.jpg');
+  width: 100%;
+  height: 100%;
 }
 </style>
