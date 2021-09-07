5c17cc51f166b3f0bce5d5efa7c63a4e48e053fc
Josh <js404839@dal.ca>
2021-09-07 12:14:02 -0700

    added in image constants

---
+++ vue-website/src/constants/constants.js
@@ -0,0 +1,13 @@
+export default {
+    images:{
+        codefresh: 'codefresh-icon.png',
+        cypress: 'cypress-icon.png',
+        docker: 'docker-icon.png',
+        flask: 'flask-icon.png',
+        github: 'github-icon.png',
+        java: 'java-icon.png',
+        linux: 'linux-icon.png',
+        mixpanel: 'mixpanel-icon.png',
+        vue: 'vue-icon.png',
+    }
+}
