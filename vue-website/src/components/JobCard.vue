acac82455f557872792ab828e9d7a155ef66b7a5
Josh <js404839@dal.ca>
2021-09-07 12:17:49 -0700

    basics of JobCard.vue

---
+++ vue-website/src/components/JobCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img :src="require(`@/assets/${image}`)">
+        <section>
+          {{description}}
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'JobCard',
+  props: {
+    description: String,
+    image: String,
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
--- vue-website/src/components/MainPage.vue
+++ vue-website/src/components/MainPage.vue
@@ -1,16 +1,31 @@
 <template>
-  <p>{{ this.msg }}</p>
+  <div class="main">
+    <JobCard :description="test" :image="codefresh"/>
+    <JobCard :description="test" :image="cypress"/>
+  </div>
 </template>
 
 <script>
+import JobCard from './JobCard.vue';
+import constants from '../constants/constants.js';
+
 export default {
   name: 'MainPage',
+  components: {
+    JobCard,
+  },
   props: {
     msg: String
-  }
+  },
+  data: function() {
+    return {
+      test: 'test description',
+      codefresh: constants.images.codefresh,
+      cypress: constants.images.cypress,
+    }
+  },
 }
 </script>
 
 <style scoped>
-
 </style>
